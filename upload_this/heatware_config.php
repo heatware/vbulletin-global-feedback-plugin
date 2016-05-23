5b4cf45fc912c785f340749e5bf7996a5b32bb12
sprakash4 <sprakash4@intuit.com>
2016-05-23 14:52:26 -0700

    Some tweaks

--- upload_this/heatware_config.php
+++ upload_this/heatware_config.php
@@ -1,17 +1,19 @@
 <?
 
-/* Modify these parameters only BEFORE installing this plugin */
+$API_KEY = ''; // REQUIRED: Enter the API key that you obtained from www.heatware.com
+
 $preInstall = array();
+$hwconfig = array();
+
+/* DO NOT MODIFY PARAMETERS AFTER INSTALLING THE PLUGIN */
 $preInstall['profile_field_label'] = "HeatWare Feedback";
 $preInstall['profile_tab_name'] = "HeatWare Feedback";
 $preInstall['profile_field_description'] = "Display your HeatWare information in your forum profile?";
 
-$hwconfig = array();
+
 $hwconfig['stats_table'] = 'hw_stats';
-$hwconfig['api_key'] = '111';
+$hwconfig['api_key'] = $API_KEY;
 $hwconfig['api_find_user'] = 'http://local.heatware.com/api/findUser';
 $hwconfig['api_get_stats'] = 'http://local.heatware.com/api/user';
 $hwconfig['api_phone_home'] = 'http://local.heatware.com/api/phoneHome';
-
-
 $configAll = array_merge($preInstall, $hwconfig);
